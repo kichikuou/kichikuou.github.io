cdcfd9afc78e0c14aea742f64ff6ec525e59da1e
kichikuou <kichikuou@users.noreply.github.com>
2015-07-11 20:36:51 +0900

    Add history section

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ isInstalled().then(function (installed) {
 }, function () {
     show($('.unsupported'));
 });
+$('#expand-history').addEventListener('click', expandHistory);
+$('#collapse-history').addEventListener('click', collapseHistory);
+function expandHistory(event) {
+    event.preventDefault();
+    hide($('#history-collapsed'));
+    show($('#history-expanded'));
+}
+function collapseHistory(event) {
+    event.preventDefault();
+    show($('#history-collapsed'));
+    hide($('#history-expanded'));
+}
