753112a84451256e6c291386fb13743b7a0e57f8
kichikuou <KichikuouChrome@gmail.com>
2018-11-25 10:32:11 +0900

    Remove dead code

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,3 @@ isInstalled().then(function(installed:boolean) {
 }, function() {
     show($('.unsupported'));
 });
-
-$('#expand-history').addEventListener('click', expandHistory);
-$('#collapse-history').addEventListener('click', collapseHistory);
-
-function expandHistory(event:Event) {
-    event.preventDefault();
-    hide($('#history-collapsed'));
-    show($('#history-expanded'));
-}
-
-function collapseHistory(event:Event) {
-    event.preventDefault();
-    show($('#history-collapsed'));
-    hide($('#history-expanded'));
-}
